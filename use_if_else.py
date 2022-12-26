03b362b97535302344a378f269f2f31c02a99eae
tejaswitaw <tejaswitawakhure@gmail.com>
2022-12-26 17:46:10 +0530

    leap year modified

--- use_if_else.py
+++ use_if_else.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     n = int(input().strip())
     if n%2 != 0:
         print("Weird")
-    elif n%2 == 0 and (n>=2 or n<=5):
+    elif n%2 == 0 and (n>=2 and n<=5):
         print("Not Weird")
-    elif n%2 == 0 and (n>=6 or n<=20):
+    elif n%2 == 0 and (n>=6 and n<=20):
         print("Weird")
     elif n%2 == 0 and n>20:
         print("Not Weird")
