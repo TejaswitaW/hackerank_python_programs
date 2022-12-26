c468b16c81e17946480c4652ae3a796f7331ba57
tejaswitaw <tejaswitawakhure@gmail.com>
2022-12-26 16:41:06 +0530

    use of if else

---
+++ use_if_else.py
@@ -0,0 +1,19 @@
+"""
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+"""
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2 != 0:
+        print("Weird")
+    elif n%2 == 0 and (n>=2 or n<=5):
+        print("Not Weird")
+    elif n%2 == 0 and (n>=6 or n<=20):
+        print("Weird")
+    elif n%2 == 0 and n>20:
+        print("Not Weird")
